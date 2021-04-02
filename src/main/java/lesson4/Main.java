2dadd803dce1c83039a421e896000d4b7bb1a0bc
MArinna <mariypol@yandex.ru>
2021-04-02 20:52:44 +0300

    homework4

---
+++ src/main/java/lesson4/Main.java
@@ -0,0 +1,12 @@
+package lesson4;
+//Задание 4 Синеокая Марина
+public class Main {
+    public static void main(String[] args) {
+    int a=5;
+    int b=7;
+    int c=3;
+    Triangle myTriangle=new Triangle(a,b,c);
+    double s=myTriangle.countArea();
+    System.out.println("Площадь треугольника= "+s);
+}
+}
